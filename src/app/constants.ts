37b1e3d63bc97f657549a7035a5fd69119ba038d
Quốc Khánh <khanh@quocs.com>
2024-06-16 20:13:06 +0700

    😷

---
+++ src/app/constants.ts
@@ -0,0 +1,1 @@
+export const START_TIME = new Date("2024-06-13T11:00:00Z").getTime();
---
+++ src/app/utils.ts
@@ -0,0 +1,11 @@
+export const countdown = (start: number) => {
+  const now = new Date().getTime();
+  const diff = now - start;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  return `${days} days ${hours} hours ${minutes} minutes and ${seconds} seconds`;
+};
